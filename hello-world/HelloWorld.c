5676d2172b7e0a51be14b989d5a58f2de9a35eeb
Levi Yan <leviyanx@gmail.com>
2022-04-05 16:37:52 +0800

    Add hello-world

---
+++ hello-world/HelloWorld.c
@@ -0,0 +1,14 @@
+// Simple C program to display "Hello World"
+
+// Header file for input/output functions
+#include<stdio.h>
+
+// main function - 
+// where the execution of grogram begains
+int main(void)
+{
+    // print hello world
+    printf("Hello World");
+
+    return 0;
+}
